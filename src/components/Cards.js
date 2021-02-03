f5e1f4bc83aa696c8b298f66d65049e1ff9f729c
Junkim97 <53490816+Junkim97@users.noreply.github.com>
2021-02-03 11:34:42 -0800

    Changed up the file structure and finished pages

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css'
+import img9 from '../images/img-9.jpg'
+import img2 from '../images/img-2.jpg'
+import img3 from '../images/img-3.jpg'
+import img4 from '../images/img-4.jpg'
+import img8 from '../images/img-8.jpg'
 function Cards() {
   return (
     <div className='cards'>
@@ -8,16 +13,16 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem src="images/img-9.jpg" text="Explore the hidden waterfall deep inside the Amazon Jungle" label='Adventure' path='/services'/>
+            <CardItem src={img9} text="Explore the hidden waterfall deep inside the Amazon Jungle" label='Adventure' path='/services'/>
 
-            <CardItem src="images/img-2.jpg" text="Travel through the islands of Bali in a private cruise" label='Luxury' path='/services'/>
+            <CardItem src={img2} text="Travel through the islands of Bali in a private cruise" label='Luxury' path='/services'/>
           </ul>
           <ul className="cards__items">
-            <CardItem src="images/img-3.jpg" text="Set sail in the Atlantic ocean visiting uncharted waters" label='Mystery' path='/services'/>
+            <CardItem src={img3} text="Set sail in the Atlantic ocean visiting uncharted waters" label='Mystery' path='/services'/>
 
-            <CardItem src="images/img-4.jpg" text="Experience football on top of the Himilayan mountains" label='Adventure' path='/products'/>
+            <CardItem src={img4} text="Experience football on top of the Himilayan mountains" label='Adventure' path='/products'/>
 
-            <CardItem src="images/img-8.jpg" text="Ride through the Sahara desert on a guided camel tour" label='Adrenaline' path='/sign-up'/>
+            <CardItem src={img8} text="Ride through the Sahara desert on a guided camel tour" label='Adrenaline' path='/sign-up'/>
           </ul>
         </div>
       </div>

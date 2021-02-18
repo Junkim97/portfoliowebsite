def50d8bbc7723b36e9b78af3ade4d84055cd1a9
Junkim97 <53490816+Junkim97@users.noreply.github.com>
2021-02-18 12:11:51 -0800

    updated about me page

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -6,8 +6,10 @@ function Services () {
   return (
     <div className='aboutContainer'>
       <h1>About Me</h1>
-      <p>I am a 4th year computer science student at the University of British Columbia</p>
-      <img src={me} height='120px' width='120px'></img>
+      <div className='textWrapper'>
+        <img src={me} height='120px' width='120px'></img>
+        <p>I am a fourth year computer science student at the University of British Columbia, and an aspiring software engineer. I mostly have experience with front end devlopment and full stack applications but I am always trying to learn new skills to add to my repetoire while polishing ones I already have. Outside of coding I enjoy photography and love to travel.</p>        
+       </div>     
     </div>
       
       

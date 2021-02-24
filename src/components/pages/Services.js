5ac5e591a89dff16a79ff089c60bd7f510345ad9
Junkim97 <53490816+Junkim97@users.noreply.github.com>
2021-02-24 11:58:36 -0800

    finished about page

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -6,10 +6,21 @@ function Services () {
   return (
     <div className='aboutContainer'>
       <h1>About Me</h1>
+      <img src={me} height='120px' width='120px'></img>
       <div className='textWrapper'>
-        <img src={me} height='120px' width='120px'></img>
-        <p>I am a fourth year computer science student at the University of British Columbia, and an aspiring software engineer. I mostly have experience with front end devlopment and full stack applications but I am always trying to learn new skills to add to my repetoire while polishing ones I already have. Outside of coding I enjoy photography and love to travel.</p>        
+        <p>I am a fourth year computer science student at the University of British Columbia, and an aspiring software engineer. I mostly have experience with front end devlopment and full stack applications but I am always trying to learn new skills to add to my repetoire while polishing ones I already have. Outside of coding I love photography and travelling.</p>        
        </div>     
+       
+       <div className='skillsWrapper'>
+         <h2>Skills</h2>
+         <div className='skillIcons'>
+           <i class='fab fa-html5'></i>
+           <i class="fab fa-css3"></i>
+           <i class="fab fa-js"></i>
+           <i class="fab fa-react"></i>
+           <i class="fab fa-python"></i>
+         </div>
+        </div>
     </div>
       
       

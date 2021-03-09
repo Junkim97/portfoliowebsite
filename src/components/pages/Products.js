7be75aee73ee3139715f0a9b09bd0b061b404972
Junkim97 <53490816+Junkim97@users.noreply.github.com>
2021-03-09 13:43:13 -0800

    started on the projects page

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -2,5 +2,20 @@ import React from 'react'
 import '../../App.css'
 
 export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>
+  return(
+    <div className='projectsWrapper'>
+      <h1>Projects</h1>
+      <div className='project1'>
+        <h2>Parking Lot Web App</h2>
+        <p>Created a webapp for an imaginary parking lot company where they could manage parking spots, and parking passes etc. Designed a schema for a relational database and used mysql to query the database and displayed the information on the browswer using html, css, and php.</p>
+      </div>
+
+
+
+
+
+
+
+    </div>
+  )
 }
